143771d0746e177a267a0e44529a8bced51a8cb0
Michael Limiero <mike5713@gmail.com>
2013-09-21 01:50:23 -0400

    Add jmdns (compiles but doesn't work yet)

--- src/net/miscjunk/aamp/MainActivity.java
+++ src/net/miscjunk/aamp/MainActivity.java
@@ -1,15 +1,35 @@
 package net.miscjunk.aamp;
 
+import java.io.IOException;
+
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import javax.jmdns.*;
 
+public class MainActivity extends Activity {
+    ServiceInfo serviceInfo;
+    JmDNS jmdns;
+     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // DEBUG - never ever do this in real life
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        serviceInfo = ServiceInfo.create("_test._tcp.local.","AndroidTest",0,"test from android");
+        try {
+            jmdns = JmDNS.create();
+            jmdns.registerService(serviceInfo);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -19,4 +39,17 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (jmdns != null) {
+            jmdns.unregisterAllServices();
+            try {
+                jmdns.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 }
