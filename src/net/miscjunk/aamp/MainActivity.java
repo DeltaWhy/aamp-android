5db39617d57f0a1b7266fc85d9fd8c2b4d290afa
Michael Limiero <mike5713@gmail.com>
2013-09-21 16:09:03 -0400

    Android server :heart_eyes:

--- src/net/miscjunk/aamp/MainActivity.java
+++ src/net/miscjunk/aamp/MainActivity.java
@@ -10,26 +10,10 @@ import android.view.Menu;
 import javax.jmdns.*;
 
 public class MainActivity extends Activity {
-    ServiceInfo serviceInfo;
-    JmDNS jmdns;
-     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // DEBUG - never ever do this in real life
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        serviceInfo = ServiceInfo.create("_test._tcp.local.","AndroidTest",0,"test from android");
-        try {
-            jmdns = JmDNS.create();
-            jmdns.registerService(serviceInfo);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -37,19 +21,5 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (jmdns != null) {
-            jmdns.unregisterAllServices();
-            try {
-                jmdns.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
     }
 }
